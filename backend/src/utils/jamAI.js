35dd3bacca0f8af0a5ef2c1c2004883591118ed3
Shin <behshinyeong@gmail.com>
2025-11-25 02:18:55 +0800

    changes

--- backend/src/utils/jamAI.js
+++ backend/src/utils/jamAI.js
@@ -1,13 +1,13 @@
 import JamAI from "jamaibase";
 
 const jamai = new JamAI({
-  token: "", //removeeeeeeeeeeee before commit
-  projectId: "" //removeeeeeeeeeeeeeee before commit
+  token: "", //removeeeeeeeeeeeeeeeee before commit
+  projectId: "" //removeeeeeeeeeeeeeeeee before commit
 });
 
 export async function addTextRow() {
   try {
-    const result = await jamai.table.addRowStream({
+    const result = await jamai.table.addRow({
       table_type: "action",
       table_id: "text-detect-scam",
       data: [{
@@ -38,6 +38,9 @@ export async function addTextRow() {
 
     console.log("✅ Final Row Output:", firstRow);
     console.log("📝 Justification / Scam Detection:", aiOutput);*/
+    const aiOutput = result.rows[0].columns['justification-scam'].choices[0].message.content;
+    console.log("✅ AI Output Text:", aiOutput);
+
 
   } catch (err) {
     console.error("❌ JamAI API Message:", err.message);
