055cc5f37bc332f00e94f36da48911a383690834
Shin <behshinyeong@gmail.com>
2025-11-26 15:21:34 +0800

    organised frontend (one centralized place to put frontend url)

---
+++ frontend/lib/services/ip.dart
@@ -0,0 +1,10 @@
+// REPLACE WITH YOUR IP.
+// Android Emulator: 'http://10.0.2.2:3000'
+// iOS Simulator: 'http://localhost:3000'
+// Physical Device / run via USB debugging: (Your PC's IP):3000
+  // know your laptop's IP address by running `ipconfig` in cmd (Windows)
+  // note that everytime you reconnect the wifi, the IP might change
+
+String port = '3000';
+String ip = 'http://192.168.0.5';
+String configBaseUrl = ip + ':' + port;
